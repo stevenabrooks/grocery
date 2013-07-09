e1193328aeedc71fd526db1614aa2b335e7eb30a
Steven Brooks <brooks.stevena@gmail.com>
2013-07-09 11:45:27 -0400

    started grocery ass in 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grocery::Application.config.secret_token = '9af46768ec1758e7877da8ba18aaa1573239f5b755427be2b504717b0a9fea1ba088fd6c9b30cb6ee1422c6690833113072dc6b17af4dc0c1402d3ca36a3db65'
